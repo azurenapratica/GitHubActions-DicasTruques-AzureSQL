3d6437e0b415f74f3b34521c182fb5a00b86ba71
Vinicius Moura <v-vimou@microsoft.com>
2021-01-17 22:42:09 -0300

    Build e Deploy de Azure SQL

---
+++ GHA-SSDT/Funcionarios.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Funcionarios] (
+    [Id]             INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]           VARCHAR (50) NOT NULL,
+    [NumeroCracha]   NCHAR (10)   NOT NULL,
+    [Endereco]       VARCHAR (50) NULL,
+    [Telefone]       VARCHAR (50) NULL,
+    [CPF]            VARCHAR (11) NULL,
+    [RG]             VARCHAR (10) NULL,
+    [Celular]        VARCHAR (12) NULL,
+    [numeroendereco] VARCHAR (8)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
